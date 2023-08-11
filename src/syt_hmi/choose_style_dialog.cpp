8c2e414a3bd5117c6decac4c7318748675c74f74
DinoWhite <dinowhite@foxmail.com>
2023-08-11 21:04:55 +0800

    feat

--- src/syt_hmi/choose_style_dialog.cpp
+++ src/syt_hmi/choose_style_dialog.cpp
@@ -6,6 +6,10 @@ ChooseStyleDialog::ChooseStyleDialog(QWidget *parent) : QDialog(parent), ui(new 
   this->setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
   this->setModal(true); // 设置为模态 父对象控件不可选
 
+  pop_menu_   = new QMenu(this);
+  delete_act_ = new QAction("删除", this);
+  pop_menu_->addAction(delete_act_);
+
   ui->style_path_btn->setParentEnabled(true);
   ui->style_path_btn->setForeEnabled(false);
   ui->style_path_btn->setStyleSheet("qproperty-press_color: rgba(0,0,100,0.5);");
@@ -48,6 +52,9 @@ ChooseStyleDialog::ChooseStyleDialog(QWidget *parent) : QDialog(parent), ui(new 
   connect(ui->return_btn, &QPushButton::clicked, [=]() {
     this->reject();
   });
+
+  connect(delete_act_, &QAction::triggered, this, &ChooseStyleDialog::deleteStyleFile);
+  connect(ui->style_file_list_view, &QListView::customContextMenuRequested, this, &ChooseStyleDialog::showContextMenu);
 }
 
 ChooseStyleDialog::~ChooseStyleDialog() {
@@ -82,4 +89,22 @@ void ChooseStyleDialog::slotSetCurrentStyleFinish(bool result) {
   } else {
     showMessageBox(this, WARN, "设置样式失败！", 1, {"确认"});
   }
+}
+
+void ChooseStyleDialog::deleteStyleFile() {
+  QString file_name = ui->style_file_list_view->model()->data(ui->style_file_list_view->currentIndex()).toString();
+  int ret = showMessageBox(this, WARN, "确认删除？", 2, {"确认", "取消"});
+
+  switch (ret) {
+  case 0:
+    QFile::remove(style_directory_ + QDir::separator() + file_name);
+    break;
+  case 1:
+    break;
+  }
+}
+
+void ChooseStyleDialog::showContextMenu(const QPoint &pos) {
+  Q_UNUSED(pos);
+  pop_menu_->exec(QCursor::pos());
 }
